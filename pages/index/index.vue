4eac47605f38519ad2c1536b39346b7ec8c85c58
Ansxu <835858556@qq.com>
2020-04-28 14:52:42 +0800

    初始化目录

---
+++ pages/index/index.vue
@@ -0,0 +1,25 @@
+<template>
+	<view class="home">
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	
+</style>
